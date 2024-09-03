13cef78844434989efcc17eefba6b9630495bf87
Hamza <mhz15019@gmail.com>
2024-09-03 22:36:05 +0500

    key security problem

--- schooltech/context_processors.py
+++ schooltech/context_processors.py
@@ -1,8 +1,12 @@
 # myapp/context_processors.py
-from django.conf import settings
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def google_maps_context(request):
     return {
-        'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY,
-        'GOOGLE_MAPS_MAP_ID': settings.GOOGLE_MAPS_MAP_ID,
+        'GOOGLE_MAPS_API_KEY':  os.getenv('GOOGLE_MAPS_API_KEY'),
+        'GOOGLE_MAPS_MAP_ID': os.getenv('GOOGLE_MAPS_MAP_ID'),
     }
